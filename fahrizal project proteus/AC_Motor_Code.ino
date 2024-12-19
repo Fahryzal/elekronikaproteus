24205d40d4e4b6944684b7d0bd5658237a0815e1
Fahrizal <109601014+Fahryzal@users.noreply.github.com>
2024-12-19 16:25:28 +0700

    Add files via upload

---
+++ fahrizal project proteus/AC_Motor_Code.ino
@@ -0,0 +1,17 @@
+void setup() 
+{
+  pinMode(13,OUTPUT);  // Relay Pin as a OUTPUT
+  pinMode(A0,INPUT_PULLUP);  // Button Pin as a INPUT PULLUP
+}
+
+void loop() 
+{
+  if(digitalRead(A0)==LOW)  // If Button is Pressed
+  {
+    digitalWrite(13,HIGH);  // Turn ON the Relay to Start the Motor
+  }
+  else
+  {
+    digitalWrite(13,LOW);  // Turn OFF the Relay to Stop the Motor
+  }
+}
